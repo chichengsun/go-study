3dab24c5d0e0705784a39cb849445b51737783bb
sunsheng1 <sunsheng1@xiaomi.com>
2025-12-28 17:20:49 +0800

    base type

---
+++ base-type/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/* 基本数据类型
+	 * 整数：
+	 * int8
+	 * int16
+	 * int32
+	 * int64
+	 * int
+	 * uint
+	 * uint8 byte别名（单字符类型）
+	 * uint16
+	 * uint32 rune别名（多字符类型）
+	 * uint64
+	 *
+	 * 浮点数：
+	 * float32
+	 * float64
+	 *
+	 * 字符串：
+	 * string
+	 *
+	 * bool：
+	 * bool true false
+	 */
+
+	// 基本数据类型变量仅声明不赋值，那么变量的值就是类型的零值，比如int是0，bool是false，string是""
+
+	fmt.Println("基本数据类型")
+}
